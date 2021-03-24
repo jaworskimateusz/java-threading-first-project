8dce8ce20bcaf37615852e38c5bc5719ae18218d
jaworski <jaworskimateuszm@gmail.com>
2021-03-24 20:12:05 +0100

    Send first N emails

--- src/main/java/com/jaworskimateuszm/firstproject/service/EmailService.java
+++ src/main/java/com/jaworskimateuszm/firstproject/service/EmailService.java
@@ -4,18 +4,18 @@ import com.jaworskimateuszm.firstproject.dto.EmailDto;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @Service
 public class EmailService {
 
     protected final int N = 10; // number of elements for one thread
 
-    protected List<EmailDto> emails = Collections.synchronizedList(new ArrayList<>());
+    protected List<EmailDto> emails = new ArrayList<>();
 
-    public synchronized boolean addEmails(List<EmailDto> emailList) {
+    public boolean addEmails(List<EmailDto> emailList) {
         return emails.addAll(emailList);
     }
 
@@ -23,16 +23,23 @@ public class EmailService {
         return emails;
     }
 
-    protected synchronized void sendEmail(String threadName) {
+    protected List<EmailDto> getFirstNMails() {
+        int elements = Math.min(emails.size(), N);
+        List<EmailDto> emailList = emails.stream().limit(elements).collect(Collectors.toList());
+        emails.removeAll(emailList);
+        return emailList;
+    }
+
+    protected void sendEmail(List<EmailDto> emailList, String threadName) {
+        emailList.forEach(this::send);
+        System.out.println(threadName + " send " + emailList.size());
+    }
+
+    protected void send(EmailDto email) {
         try {
-            while(emails.size() > 0) {
-                //TODO https://www.baeldung.com/java-email maybe in future developement
-                TimeUnit.MILLISECONDS.sleep(1000);
-                EmailDto email = emails.stream().filter(e -> !e.getStatus()).findFirst().get();
-                emails.remove(email);
-                System.out.println(threadName + " deleted " + email.toString());
-                System.out.println("Email list size now is: " + emails.size());
-            }
+            int min = 50, max = 500;
+            long sendTime = (long)(Math.random() * (max - min + 1) + min);
+            TimeUnit.MILLISECONDS.sleep(sendTime);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
--- src/main/java/com/jaworskimateuszm/firstproject/service/EmailThreadService.java
+++ src/main/java/com/jaworskimateuszm/firstproject/service/EmailThreadService.java
@@ -2,23 +2,16 @@ package com.jaworskimateuszm.firstproject.service;
 
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Service
 public class EmailThreadService extends EmailService {
 
-    List<Thread> threads = new ArrayList<>();
-
     public void createThreads() {
-        int threadsNumber = emails.size() / N;
+        int threadsNumber = emails.size() / N + 1;
         for (int i = 0; i < threadsNumber; i++) {
             Thread thread = new Thread(() ->
-                    sendEmail(Thread.currentThread().getName())
+                    sendEmail(getFirstNMails(), Thread.currentThread().getName())
             );
             thread.start();
-            threads.add(thread);
         }
     }
-
 }
